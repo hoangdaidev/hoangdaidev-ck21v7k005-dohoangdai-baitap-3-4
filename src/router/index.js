1b97611404d227d5d2f38e4668a32e5409174695
hoangdaidev <hoangdaidev@gmail.com>
2022-10-21 00:14:24 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,16 @@ import {
 } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 const routes = [{
-    path: "/",
-    name: "contactbook",
-    component: ContactBook,
-}, ];
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+];
 const router = createRouter({
     history: createWebHistory(
         import.meta.env.BASE_URL),
